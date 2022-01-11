432ce0e3d9af7e7b4e6a1c9796a82bcbe6e88313
Bozhidar Slaveykov <bobi@microweber.com>
2022-01-11 22:34:31 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -10,18 +10,27 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //},
+  networks: {
+    development: {
+       host: "127.0.0.1",     // Localhost (default: none)
+       port: 7545,            // Standard BSC port (default: none)
+       network_id: "*",       // Any network (default: none)
+     },
+     testnet: {
+       provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+       network_id: 97,
+       confirmations: 10,
+       timeoutBlocks: 200,
+       skipDryRun: true
+     },
+     bsc: {
+       provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+       network_id: 56,
+       confirmations: 10,
+       timeoutBlocks: 200,
+       skipDryRun: true
+     },
+  },
   //
   // Truffle DB is currently disabled by default; to enable it, change enabled:
   // false to enabled: true. The default storage location can also be
