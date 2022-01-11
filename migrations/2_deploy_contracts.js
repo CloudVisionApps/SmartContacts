310609921ce4236b48d036a554d39419aec9ed1a
Bozhidar Slaveykov <bobi@microweber.com>
2022-01-11 22:42:31 +0200

    update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const AFCToken = artifacts.require("AFCToken");
 
 module.exports = async function(deployer) {
-  await deployer.deploy();
+  await deployer.deploy(AFCToken);
   const instance = await AFCToken.deployed();
   if(instance) {
     console.log("AFCToken successfully deployed.")
--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -2,7 +2,7 @@
 const Lottery = artifacts.require("Lottery");
 
 module.exports = async function(deployer) {
-  await deployer.deploy();
+  await deployer.deploy(Lottery);
   const instance = await Lottery.deployed();
   if(instance) {
     console.log("Lottery successfully deployed.")
